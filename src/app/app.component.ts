63783b64e3d05c02bdc697b2521f48e965ee7a3c
jinalshah999 <jinalshah999@gmail.com>
2019-08-22 12:45:02 +0530

    added display message using observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChatService } from './chat.service';
 
 @Component({
@@ -6,11 +6,19 @@ import { ChatService } from './chat.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'chatdemosocket';
   message:string='';
+  messages:string[]=[];
   public constructor(private _chat:ChatService){
 
+  }
+  ngOnInit(){
+    this._chat.getMessage().subscribe(
+        (x:string)=>{
+          this.messages.push(x);
+        }
+    );
   }
   onSendClick(){
     this._chat.sendMessage(this.message);
